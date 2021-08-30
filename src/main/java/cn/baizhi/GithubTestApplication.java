6db9a808f78be86bc945067179adf065eb045fb2
JiangXiaoWang666 <79172980+JiangXiaoWang666@users.noreply.github.com>
2021-08-30 18:03:33 +0800

    Update GithubTestApplication.java

--- src/main/java/cn/baizhi/GithubTestApplication.java
+++ src/main/java/cn/baizhi/GithubTestApplication.java
@@ -9,6 +9,7 @@ public class GithubTestApplication {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("ggggggggg");
+         System.out.println("0000000000000000000000000000000000000000000000000000000");
         SpringApplication.run(GithubTestApplication.class, args);
     }
 
